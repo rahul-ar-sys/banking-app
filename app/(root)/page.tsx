a977c87ba523c35a06bb46219c60e91ecd50ac53
Rahul AR <nnm22am045@nmamit.in>
2025-02-13 00:56:00 +0530

    final commit of day 3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,11 @@ import Head from 'next/head'
 import React, { use } from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSideBar from '@/components/RightSideBar';
+import { LucideToggleLeft } from 'lucide-react';
 
 const Home = () => {
-    const loggedIn = {firstname : "John"};
+    const loggedIn = {firstName : "John" , lastName : "Doe" , email : "johndoe@gmail.com"};
 
   return (
     <section className='home'>
@@ -15,18 +17,32 @@ const Home = () => {
             <HeaderBox 
                 type="greeting"
                 title="Welcome to Horizon"
-                user={loggedIn?.firstname || 'Guest'}
+                user={loggedIn?.firstName || 'Guest'}
                 subtext="Access and manage your bank accounts efficiently"
             
             />
-        </header>
+        
 
         <TotalBalanceBox
             accounts={[]}
             totalBanks={2}
             totalCurrentBalance={1250.35}
         />
+        </header>
+
+        RECENT TRANSACTIONS
+
       </div>
+
+      <RightSideBar 
+        user={loggedIn} 
+        transactions={[]}
+        banks={[
+          { $id: '1', accountId: 'acc1', bankId: 'bank1', accessToken: 'token1', name: 'Bank 1', currentBalance: 1000 },
+          { $id: '2', accountId: 'acc2', bankId: 'bank2', accessToken: 'token2', name: 'Bank 2', currentBalance: 250.35 }
+        ]}
+      
+      />
     </section>
   )
 }
